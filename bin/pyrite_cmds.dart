bca449bb9e1e5f7831cbb09c6b5f29d49b0c1b8d
Nub <38899321+One-Nub@users.noreply.github.com>
2022-09-24 02:30:04 -0500

    Add sync script

---
+++ bin/pyrite_cmds.dart
@@ -0,0 +1,24 @@
+import 'package:dotenv/dotenv.dart';
+import 'package:lirx/lirx.dart';
+
+import 'dart:convert';
+
+void main() async {
+  var env = DotEnv()..load(['bin/.env']);
+  final String token = env["TOKEN"]!;
+  final BigInt appId = BigInt.parse(env["APP_ID"]!);
+
+  Lirx lirx = Lirx(botToken: token, applicationID: appId);
+  await lirx.loadCommandFiles([
+    'commands/about.toml',
+    'commands/config.toml',
+    'commands/help.toml',
+    'commands/redeem.toml',
+    'commands/rules.toml',
+    'commands/scan.toml',
+    'commands/transfer.toml'
+  ]);
+
+  // print(jsonEncode(lirx.commandList));
+  print(await lirx.bulkPublishCommands());
+}
