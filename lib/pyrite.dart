640db1f53cc78481e43d4d773ef5e597e49b6f5b
Nub <38899321+One-Nub@users.noreply.github.com>
2022-09-27 01:50:43 -0500

    Move webserver into backend, begin work on Pyrite class

--- lib/pyrite.dart
+++ lib/pyrite.dart
@@ -1,3 +1,21 @@
-int calculate() {
-  return 6 * 7;
+import 'package:alfred/alfred.dart';
+import 'package:onyx/onyx.dart';
+
+import 'src/backend/webserver.dart';
+
+class Pyrite {
+  final String token;
+  final String publicKey;
+  late final Onyx onyx;
+
+  Pyrite({required this.token, required this.publicKey}) {
+    onyx = Onyx();
+  }
+
+  void startGateway() async {}
+
+  void startServer() async {
+    WebServer server = WebServer(Alfred(), publicKey);
+    server.startServer(dispatchFunc: onyx.dispatchInteraction);
+  }
 }
