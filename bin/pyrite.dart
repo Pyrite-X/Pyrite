b2414ee12a30a6dfd0f863d3d6b36b45df2e705a
Nub <38899321+One-Nub@users.noreply.github.com>
2022-09-27 23:39:41 -0500

    Use Pyrite server instead of webserver.dart

--- bin/pyrite.dart
+++ bin/pyrite.dart
@@ -1,15 +1,15 @@
 import 'package:alfred/alfred.dart';
 import 'package:dotenv/dotenv.dart';
 import 'package:onyx/onyx.dart';
-import 'package:pyrite/pyrite.dart' as pyrite;
+import 'package:pyrite/pyrite.dart';
 
 import 'webserver.dart';
 
 void main(List<String> arguments) {
   var env = DotEnv()..load(['bin/.env']);
   final String publicKey = env["PUB_KEY"]!;
+  final String token = env["TOKEN"]!;
 
-  Onyx onyx = Onyx();
-  WebServer server = WebServer(Alfred(), publicKey);
-  server.startServer(dispatchFunc: (interaction) => onyx.dispatchInteraction(interaction));
+  Pyrite bot = Pyrite(token: token, publicKey: publicKey);
+  bot.startServer();
 }
