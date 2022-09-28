c778a53c765cb9c5aa6d2406e5369d91afc6962e
Nub <38899321+One-Nub@users.noreply.github.com>
2022-09-27 23:42:00 -0500

    Add about command

--- lib/pyrite.dart
+++ lib/pyrite.dart
@@ -3,6 +3,14 @@ import 'package:onyx/onyx.dart';
 
 import 'src/backend/webserver.dart';
 
+import 'src/modules/interactions/about.dart' as about;
+import 'src/modules/interactions/config.dart' as config;
+import 'src/modules/interactions/help.dart' as help;
+import 'src/modules/interactions/redeem.dart' as redeem;
+import 'src/modules/interactions/rules.dart' as rules;
+import 'src/modules/interactions/scan.dart' as scan;
+import 'src/modules/interactions/transfer.dart' as transfer;
+
 class Pyrite {
   final String token;
   final String publicKey;
@@ -10,6 +18,7 @@ class Pyrite {
 
   Pyrite({required this.token, required this.publicKey}) {
     onyx = Onyx();
+    onyx.registerAppCommandHandler("about", about.aboutCmd);
   }
 
   void startGateway() async {}
---
+++ lib/src/modules/interactions/about.dart
@@ -0,0 +1,43 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:alfred/alfred.dart';
+import 'package:nyxx/nyxx.dart' show EmbedBuilder, DiscordColor;
+import 'package:onyx/onyx.dart';
+
+void aboutCmd(Interaction interaction) async {
+  HttpRequest metadata = interaction.metadata;
+
+  var embedBuilder = EmbedBuilder();
+  embedBuilder.title = "Here's some information about Pyrite!";
+  embedBuilder.thumbnailUrl =
+      "https://cdn.discordapp.com/avatars/1022370218489692222/5966690e72baa4b1ceca1b76c49ef0ed.webp?size=2048";
+  embedBuilder.timestamp = DateTime.now();
+  embedBuilder.color = DiscordColor.fromHexString("ed6432");
+  embedBuilder.addField(
+      name: "What is Pyrite?", content: "Pyrite is a bot focused on removing phishing bots!", inline: false);
+  embedBuilder.addField(
+      name: "Why make Pyrite?",
+      content:
+          "While doing my stuff for [Bloxlink](https://blox.link), I noticed that a lot of large servers typically have many fake user accounts appearing like popular verification bots attempting to phish users (aka steal their account).\nSo just while thinking about why this is, I thought, why not make a bot that removes these accounts! And so, Pyrite came to be.",
+      inline: false);
+  embedBuilder.addField(
+      name: "What is Pyrite coded in?",
+      content:
+          "[Dart!](https://dart.dev) With some self-made frameworks of my own, specifically [Lirx](https://github.com/One-Nub/Lirx) and [Onyx](https://github.com/One-Nub/Onyx). I also use [Nyxx](https://github.com/nyxx-discord/nyxx) for some gateway functionality, as well as for object representations.",
+      inline: false);
+  embedBuilder.addFooter((footer) {
+    footer.text = "Created by: Nub#8399";
+    footer.iconUrl =
+        "https://cdn.discordapp.com/avatars/156872400145874944/8443e3f62d931e469567d27526ce57d1.webp?size=2048";
+  });
+
+  InteractionResponse response = InteractionResponse(InteractionResponseType.message_response, {
+    "embeds": [
+      {...embedBuilder.build()}
+    ]
+  });
+
+  metadata.response.headers.contentType = ContentType.json;
+  await metadata.response.send(jsonEncode(response));
+}
