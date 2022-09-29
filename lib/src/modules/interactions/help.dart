2de5a141d1cdb2b9a417083782fb4898a18a6d65
Nub <38899321+One-Nub@users.noreply.github.com>
2022-09-28 23:54:46 -0500

    Add initial help command function

--- lib/src/modules/interactions/help.dart
+++ lib/src/modules/interactions/help.dart
@@ -1,5 +1,25 @@
+import 'dart:convert';
+
+import 'package:alfred/alfred.dart';
+import 'package:nyxx/nyxx.dart' show EmbedBuilder, DiscordColor;
 import 'package:onyx/onyx.dart';
 
 void helpCmd(Interaction interaction) async {
-  //TODO
+  HttpRequest request = interaction.metadata["request"];
+
+  var embedBuilder = EmbedBuilder();
+  embedBuilder.title = "Help! I'm confused!";
+  embedBuilder.description =
+      "As Pyrite is currently in it's very alpha stages, we have no help documents, or a support server! \n"
+      "Please stay tuned as development advances, or until a support server is made.";
+  embedBuilder.timestamp = DateTime.now();
+  embedBuilder.color = DiscordColor.fromHexString("4D346D");
+
+  InteractionResponse response = InteractionResponse(InteractionResponseType.message_response, {
+    "embeds": [
+      {...embedBuilder.build()}
+    ]
+  });
+
+  await request.response.send(jsonEncode(response));
 }
