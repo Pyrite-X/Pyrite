3dce7bde475839e4d444d097e66874fa0fa4a323
Nub <38899321+One-Nub@users.noreply.github.com>
2022-09-29 23:16:36 -0500

    Reply a WIP message for rules commands

--- lib/src/modules/interactions/rules.dart
+++ lib/src/modules/interactions/rules.dart
@@ -1,5 +1,46 @@
+import 'dart:convert';
+
+import 'package:alfred/alfred.dart';
+import 'package:nyxx/nyxx.dart' show EmbedBuilder, DiscordColor;
 import 'package:onyx/onyx.dart';
 
+import '../../structures/action.dart';
+import '../../structures/rule.dart';
+
 void rulesCmd(Interaction interaction) async {
-  //TODO
+  var interactionData = interaction.data! as ApplicationCommandData;
+
+  ApplicationCommandOption subcommand = interactionData.options![0];
+  String optionName = subcommand.name;
+
+  if (optionName == "view") {
+  } else if (optionName == "add") {
+  } else if (optionName == "delete") {}
+}
+
+void viewRules(Interaction interaction) async {
+  HttpRequest request = interaction.metadata["request"];
+
+  InteractionResponse response = InteractionResponse(
+      InteractionResponseType.message_response, {"content": "Still a WIP!", "flags": 1 << 6});
+
+  request.response.send(jsonEncode(response));
+}
+
+void addRule(Interaction interaction) async {
+  HttpRequest request = interaction.metadata["request"];
+
+  InteractionResponse response = InteractionResponse(
+      InteractionResponseType.message_response, {"content": "Still a WIP!", "flags": 1 << 6});
+
+  request.response.send(jsonEncode(response));
+}
+
+void deleteRule(Interaction interaction) async {
+  HttpRequest request = interaction.metadata["request"];
+
+  InteractionResponse response = InteractionResponse(
+      InteractionResponseType.message_response, {"content": "Still a WIP!", "flags": 1 << 6});
+
+  request.response.send(jsonEncode(response));
 }
