24f51f29dc9b4dd99aa0a6513ce43709591b5656
Nub <38899321+One-Nub@users.noreply.github.com>
2022-10-01 02:25:21 -0500

    Add basic kick and ban wrapper

--- lib/src/backend/actions/ban.dart
+++ lib/src/backend/actions/ban.dart
@@ -1,1 +1,5 @@
+import '../../discord_http.dart';
 
+void banUser({required BigInt userID, required BigInt guildID, required DiscordHTTP httpClient}) async {
+  await httpClient.banUser(guildID: guildID, userID: userID);
+}
--- lib/src/backend/actions/kick.dart
+++ lib/src/backend/actions/kick.dart
@@ -1,1 +1,5 @@
+import '../../discord_http.dart';
 
+void kickUser({required BigInt userID, required BigInt guildID, required DiscordHTTP httpClient}) async {
+  await httpClient.kickUser(guildID: guildID, userID: userID);
+}
