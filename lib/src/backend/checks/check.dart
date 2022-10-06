1d24816e2360841673e6e43f2a77891702d0f7a6
Nub <38899321+One-Nub@users.noreply.github.com>
2022-10-06 17:07:16 -0500

    Implement base checkUser method

--- lib/src/backend/checks/check.dart
+++ lib/src/backend/checks/check.dart
@@ -1,6 +1,14 @@
 import '../../structures/trigger/trigger_context.dart';
 import '../../structures/trigger/trigger_source.dart';
 
+import '../../structures/scan_types.dart';
+
+import '../actions/run_actions.dart';
+
+import 'check_phish_list.dart' as phish;
+import 'check_result.dart';
+import 'check_rules.dart' as rules;
+
 void checkUser(TriggerContext context) async {
   /// Flow:
   ///   - Get server premium status (when implemented).
@@ -20,4 +28,37 @@ void checkUser(TriggerContext context) async {
   ///   - Perform actions as they match (kick, ban, log, other combination)
   ///
   ///   - Get actions through containsValue on Action w/ ActionEnum as parameter.
+
+  CheckPhishResult? checkPhishResult;
+  CheckRulesResult? checkRulesResult;
+
+  var contextSource = context.eventSource.sourceType;
+  if (contextSource == EventSourceType.join) {
+    checkPhishResult = phish.checkPhishingList(context);
+
+    // if premium and checkPhishResult has no match, check rules... for now just check rules if no match
+    if (!checkPhishResult.match) {
+      checkRulesResult = rules.checkRulesList(context);
+    }
+  }
+
+  if (contextSource == EventSourceType.scan) {
+    ScanMode scanningMode = context.eventSource.scanningMode!;
+
+    if (scanningMode.containsType(ScanModeOptions.phish)) {
+      checkPhishResult = phish.checkPhishingList(context);
+    }
+
+    if (scanningMode.containsType(ScanModeOptions.rules)) {
+      checkRulesResult = rules.checkRulesList(context);
+    }
+  }
+
+  if (checkPhishResult != null && checkPhishResult.match) {
+    runActions(context, checkPhishResult);
+  }
+
+  if (checkRulesResult != null && checkRulesResult.match) {
+    runActions(context, checkRulesResult);
+  }
 }
