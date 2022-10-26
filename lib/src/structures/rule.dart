3aaddea1b689cda2a535652846bba3f058c3c6f1
Nub <38899321+One-Nub@users.noreply.github.com>
2022-10-26 01:41:18 -0500

    Require ruleID, add ruleID generation to RuleBuilder, req authorID

--- lib/src/structures/rule.dart
+++ lib/src/structures/rule.dart
@@ -1,23 +1,26 @@
-import 'package:pyrite/src/structures/action.dart';
+import 'dart:math';
+
+import './action.dart';
 
 class Rule {
-  String? ruleID;
-  BigInt? authorID;
+  String ruleID;
+  BigInt authorID;
   bool regex;
   String pattern;
   Action action;
   List<BigInt>? excludedRoles;
 
   Rule(
-      {required this.action,
+      {required this.ruleID,
+      required this.action,
+      required this.authorID,
       required this.pattern,
-      required this.regex,
-      this.authorID,
-      this.excludedRoles,
-      this.ruleID});
+      this.regex = false,
+      this.excludedRoles});
 }
 
 class RuleBuilder {
+  late String ruleID;
   late Action action;
   late BigInt authorID;
   List<BigInt> excludedRoles = [];
@@ -32,6 +35,21 @@ class RuleBuilder {
   void setPattern(String pattern) => this.pattern = pattern;
   void setRegexFlag(bool flag) => regex = flag;
 
-  Rule build() =>
-      Rule(action: action, authorID: authorID, excludedRoles: excludedRoles, pattern: pattern, regex: regex);
+  void setRuleID(String ruleID) => this.ruleID = ruleID;
+  void generateRuleID() {
+    var elements = List<String>.generate(4, (index) => Random.secure().nextInt(256).toRadixString(16));
+    StringBuffer stringBuffer = StringBuffer();
+    elements.forEach((element) {
+      stringBuffer.write(element);
+    });
+    ruleID = stringBuffer.toString();
+  }
+
+  Rule build() => Rule(
+      ruleID: ruleID,
+      action: action,
+      authorID: authorID,
+      excludedRoles: excludedRoles,
+      pattern: pattern,
+      regex: regex);
 }
