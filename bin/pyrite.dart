e2c299caff8f7f825b9983042606f831e2ec1e0c
Nub <38899321+One-Nub@users.noreply.github.com>
2022-11-23 03:23:11 -0600

    Start database connections

--- bin/pyrite.dart
+++ bin/pyrite.dart
@@ -1,5 +1,6 @@
 import 'package:dotenv/dotenv.dart';
 import 'package:pyrite/pyrite.dart';
+import 'package:pyrite/src/backend/database.dart';
 import 'package:pyrite/src/discord_http.dart';
 
 void main(List<String> arguments) {
@@ -8,8 +9,13 @@ void main(List<String> arguments) {
   final String publicKey = env["PUB_KEY"]!;
   final String token = env["TOKEN"]!;
 
-  /// Simply instantiate DiscordHTTP.
+  /// Initialize DiscordHTTP with custom settings
   DiscordHTTP(authToken: token, applicationID: appID, discordURL: "192.168.254.108", scheme: "http");
+
+  /// Start the database connection.
+  DatabaseClient(initializing: true);
+
+  /// Start bot features.
   Pyrite bot = Pyrite(token: token, publicKey: publicKey, appID: appID);
   bot.startServer();
   bot.startGateway();
