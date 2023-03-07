0c355fc387834805e21da10524cec70fd832b56b
Nub <38899321+One-Nub@users.noreply.github.com>
2023-03-07 00:35:06 -0600

    Remove excluded fields option on Rule & RuleBuilder

--- lib/src/structures/rule.dart
+++ lib/src/structures/rule.dart
@@ -8,28 +8,24 @@ class Rule {
   bool regex;
   String pattern;
   Action action;
-  List<BigInt>? excludedRoles;
 
   Rule(
       {required this.ruleID,
       required this.action,
       required this.authorID,
       required this.pattern,
-      this.regex = false,
-      this.excludedRoles});
+      this.regex = false});
 }
 
 class RuleBuilder {
   late String ruleID;
   late Action action;
   late BigInt authorID;
-  List<BigInt> excludedRoles = [];
   late String pattern;
   bool regex = false;
 
   RuleBuilder();
 
-  void addExcludedRole(BigInt roleID) => excludedRoles.add(roleID);
   void setAction(Action action) => this.action = action;
   void setAuthorID(BigInt authorID) => this.authorID = authorID;
   void setPattern(String pattern) => this.pattern = pattern;
@@ -45,11 +41,5 @@ class RuleBuilder {
     ruleID = stringBuffer.toString();
   }
 
-  Rule build() => Rule(
-      ruleID: ruleID,
-      action: action,
-      authorID: authorID,
-      pattern: pattern,
-      regex: regex,
-      excludedRoles: excludedRoles);
+  Rule build() => Rule(ruleID: ruleID, action: action, authorID: authorID, pattern: pattern, regex: regex);
 }
