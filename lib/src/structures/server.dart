712528d052ccf3c8ff83cf6b4de6efd8213029c3
Nub <38899321+One-Nub@users.noreply.github.com>
2023-03-07 15:23:03 -0600

    Include various database fields on Server object

--- lib/src/structures/server.dart
+++ lib/src/structures/server.dart
@@ -1,12 +1,25 @@
+import 'action.dart';
 import 'rule.dart';
 
 class Server {
   BigInt serverID;
   BigInt ownerID;
   BigInt? logchannelID;
+  bool? onJoinEnabled;
+  int? fuzzyMatchPercent;
+  bool? checkPhishingList;
+  Action? phishingMatchAction;
   List<Rule> rules = [];
 
-  Server({required this.serverID, required this.ownerID, this.logchannelID, List<Rule>? rules}) {
+  Server(
+      {required this.serverID,
+      required this.ownerID,
+      this.logchannelID,
+      this.onJoinEnabled,
+      this.fuzzyMatchPercent,
+      this.checkPhishingList,
+      this.phishingMatchAction,
+      List<Rule>? rules}) {
     if (rules != null) this.rules = rules;
   }
 }
@@ -15,6 +28,10 @@ class ServerBuilder {
   late BigInt serverID;
   late BigInt ownerID;
   BigInt? logchannelID;
+  bool? onJoinEnabled;
+  int? fuzzyMatchPercent;
+  bool? checkPhishingList;
+  Action? phishingMatchAction;
   List<Rule> rules = [];
 
   ServerBuilder();
@@ -25,7 +42,23 @@ class ServerBuilder {
 
   void setLogChannelID(BigInt logchannelID) => this.logchannelID = logchannelID;
 
+  void setOnJoinEnabled(bool onJoinEnabled) => this.onJoinEnabled = onJoinEnabled;
+
+  void setFuzzyMatchPercent(int value) => this.fuzzyMatchPercent = value;
+
+  void setPhishingListChecking(bool value) => this.checkPhishingList = value;
+
+  void setPhishingMatchAction(Action action) => this.phishingMatchAction = action;
+
   void addRule(Rule rule) => rules.add(rule);
 
-  Server build() => Server(serverID: serverID, ownerID: ownerID, logchannelID: logchannelID, rules: rules);
+  Server build() => Server(
+      serverID: serverID,
+      ownerID: ownerID,
+      logchannelID: logchannelID,
+      onJoinEnabled: onJoinEnabled,
+      fuzzyMatchPercent: fuzzyMatchPercent,
+      checkPhishingList: checkPhishingList,
+      phishingMatchAction: phishingMatchAction,
+      rules: rules);
 }
