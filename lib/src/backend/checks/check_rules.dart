78032a6cbd152593a3f06b09ede0babebbb2f791
Nub <38899321+One-Nub@users.noreply.github.com>
2023-03-07 16:42:07 -0600

    Implement checkRulesList logic

--- lib/src/backend/checks/check_rules.dart
+++ lib/src/backend/checks/check_rules.dart
@@ -1,11 +1,42 @@
 import '../../structures/rule.dart';
+import '../../structures/user.dart';
 import '../../structures/trigger/trigger_context.dart';
 import 'check_result.dart';
 
 import '../../backend/database.dart' as db;
 
 Future<CheckRulesResult> checkRulesList(TriggerContext context) async {
-  List<dynamic> serverRuleList = await db.fetchGuildRules(serverID: context.server.serverID);
+  List<Rule> ruleList = context.server.rules;
+  if (context.server.rules.isEmpty) {
+    List<dynamic> serverRuleList = await db.fetchGuildRules(serverID: context.server.serverID);
+    if (serverRuleList.isEmpty) {
+      return CheckRulesResult(match: false);
+    }
+    serverRuleList.forEach((element) => ruleList.add(Rule.fromJson(element)));
+  }
 
-  return CheckRulesResult(match: false);
+  User user = context.user;
+  Rule? matchRule;
+  for (Rule rule in ruleList) {
+    if (rule.regex) {
+      RegExp pattern = RegExp(rule.pattern);
+      RegExpMatch? usernameMatch = pattern.firstMatch(user.username);
+      RegExpMatch? nicknameMatch = pattern.firstMatch(user.nickname!.toString());
+      if (usernameMatch != null || nicknameMatch != null) {
+        matchRule = rule;
+        break;
+      }
+    } else {
+      bool usernameCheck = user.username.toLowerCase() == rule.pattern.toLowerCase();
+      bool nicknameCheck = user.nickname?.toLowerCase() == rule.pattern.toLowerCase();
+      if (usernameCheck || nicknameCheck) {
+        matchRule = rule;
+        break;
+      }
+    }
+  }
+
+  return (matchRule == null)
+      ? CheckRulesResult(match: false)
+      : CheckRulesResult(match: true, rule: matchRule);
 }
