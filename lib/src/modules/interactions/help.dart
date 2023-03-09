7ba7c1345853646a64c881ee0dcaa6c00d6d192b
Nub <38899321+One-Nub@users.noreply.github.com>
2023-03-09 00:13:54 -0600

    Update description wording + use base_embeds

--- lib/src/modules/interactions/help.dart
+++ lib/src/modules/interactions/help.dart
@@ -1,19 +1,20 @@
 import 'dart:convert';
 
 import 'package:alfred/alfred.dart';
-import 'package:nyxx/nyxx.dart' show EmbedBuilder, DiscordColor;
+import 'package:nyxx/nyxx.dart' show EmbedBuilder, EmbedFooterBuilder;
 import 'package:onyx/onyx.dart';
+
+import '../../utilities/base_embeds.dart' as embeds;
 
 void helpCmd(Interaction interaction) async {
   HttpRequest request = interaction.metadata["request"];
 
-  var embedBuilder = EmbedBuilder();
+  EmbedBuilder embedBuilder = embeds.warningEmbed();
   embedBuilder.title = "Help! I'm confused!";
   embedBuilder.description =
-      "As Pyrite is currently in it's very alpha stages, we have no help documents, or a support server! \n"
-      "Please stay tuned as development advances, or until a support server is made.";
-  embedBuilder.timestamp = DateTime.now();
-  embedBuilder.color = DiscordColor.fromHexString("4D346D");
+      "As Pyrite is currently in it's very alpha stages, there are no help documents, or a support server quite yet! \n\n"
+      "Please stay tuned as development advances, a support server will be made and linked here soon!";
+  embedBuilder.footer = EmbedFooterBuilder(text: "Guild ID: ${interaction.guild_id.toString()}");
 
   InteractionResponse response = InteractionResponse(InteractionResponseType.message_response, {
     "embeds": [
