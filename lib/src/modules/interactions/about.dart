93fd04e04cf2f0fd6100ea845e651dbed67093d8
Nub <38899321+One-Nub@users.noreply.github.com>
2023-03-09 01:10:17 -0600

    Use base_embeds for about.dart

--- lib/src/modules/interactions/about.dart
+++ lib/src/modules/interactions/about.dart
@@ -1,18 +1,18 @@
 import 'dart:convert';
 
 import 'package:alfred/alfred.dart';
-import 'package:nyxx/nyxx.dart' show EmbedBuilder, DiscordColor;
+import 'package:nyxx/nyxx.dart' show EmbedBuilder;
 import 'package:onyx/onyx.dart';
+
+import '../../utilities/base_embeds.dart' as embeds;
 
 void aboutCmd(Interaction interaction) async {
   HttpRequest request = interaction.metadata["request"];
 
-  var embedBuilder = EmbedBuilder();
+  var embedBuilder = embeds.infoEmbed();
   embedBuilder.title = "Here's some information about Pyrite!";
   embedBuilder.thumbnailUrl =
       "https://cdn.discordapp.com/avatars/1022370218489692222/5966690e72baa4b1ceca1b76c49ef0ed.webp?size=2048";
-  embedBuilder.timestamp = DateTime.now();
-  embedBuilder.color = DiscordColor.fromHexString("4D346D");
   embedBuilder.addField(
       name: "What is Pyrite?", content: "Pyrite is a bot focused on removing phishing bots!", inline: false);
   embedBuilder.addField(
@@ -26,7 +26,7 @@ void aboutCmd(Interaction interaction) async {
           "[Dart!](https://dart.dev) With some self-made frameworks of my own, specifically [Lirx](https://github.com/One-Nub/Lirx) and [Onyx](https://github.com/One-Nub/Onyx). I also use [Nyxx](https://github.com/nyxx-discord/nyxx) for some gateway functionality, as well as for object representations.",
       inline: false);
   embedBuilder.addFooter((footer) {
-    footer.text = "Created by: Nub#8399";
+    footer.text = "Created by: Nub#8399 | Guild ID: ${interaction.guild_id}";
     footer.iconUrl =
         "https://cdn.discordapp.com/avatars/156872400145874944/8443e3f62d931e469567d27526ce57d1.webp?size=2048";
   });
