92821b2ee8d48414a8e5ad0c3a0e5b203cea1e78
Nub <38899321+One-Nub@users.noreply.github.com>
2023-03-11 22:39:21 -0600

    Add invite command

---
+++ lib/src/modules/interactions/invite.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+import 'package:alfred/alfred.dart';
+import 'package:nyxx/nyxx.dart' show EmbedBuilder, EmbedFooterBuilder;
+import 'package:onyx/onyx.dart';
+
+import '../../utilities/base_embeds.dart' as embeds;
+
+void helpCmd(Interaction interaction) async {
+  HttpRequest request = interaction.metadata["request"];
+
+  EmbedBuilder embedBuilder = embeds.warningEmbed();
+  embedBuilder.title = "Invite Pyrite!";
+  embedBuilder.description =
+      "Invite Pyrite from this url: https://discord.com/api/oauth2/authorize?client_id=1022370218489692222&permissions=1374926720198&scope=bot%20applications.commands";
+  embedBuilder.footer = EmbedFooterBuilder(text: "Guild ID: ${interaction.guild_id.toString()}");
+
+  InteractionResponse response = InteractionResponse(InteractionResponseType.message_response, {
+    "embeds": [
+      {...embedBuilder.build()}
+    ]
+  });
+
+  await request.response.send(jsonEncode(response));
+}
