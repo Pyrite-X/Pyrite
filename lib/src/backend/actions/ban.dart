1cf3722f3fff932b1aa963902a3a26860b853c55
Nub <38899321+One-Nub@users.noreply.github.com>
2023-03-16 21:03:28 -0500

    Update kick & ban audit log reasons to have more detail

--- lib/src/backend/actions/ban.dart
+++ lib/src/backend/actions/ban.dart
@@ -5,12 +5,18 @@ import '../../structures/trigger/trigger_context.dart';
 
 void banUser({required TriggerContext context, CheckResult? result}) async {
   String logReason = "";
+  var user = context.user;
 
   if (result is CheckPhishResult) {
-    logReason = "User was banned for having a name similar to ${result.matchingString}";
+    String percentage =
+        (result.fuzzyMatchPercent == 100) ? "100" : result.fuzzyMatchPercent!.toStringAsPrecision(4);
+    logReason = "${user.tag} (${user.userID}) - "
+        "Username/Nickname matched \"${result.matchingString}\" from the bot list "
+        "as a ~$percentage% match.";
   } else if (result is CheckRulesResult) {
-    logReason =
-        "User was banned because they matched the pattern ${result.rule!.pattern} on ${result.rule!.ruleID}";
+    logReason = "${user.tag} (${user.userID}) - "
+        "Username/Nickname matched Rule \"${result.rule!.ruleID}\" with "
+        "the pattern ${result.rule!.pattern}.";
   }
 
   if (logReason.isEmpty) {
