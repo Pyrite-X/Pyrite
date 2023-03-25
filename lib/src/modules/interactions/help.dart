a60d573616abfda4af7dfe133b31fb084901ee67
Nub <38899321+One-Nub@users.noreply.github.com>
2023-03-25 01:02:41 -0500

    Update help embed body with invite to support server

--- lib/src/modules/interactions/help.dart
+++ lib/src/modules/interactions/help.dart
@@ -12,8 +12,7 @@ void helpCmd(Interaction interaction) async {
   EmbedBuilder embedBuilder = embeds.warningEmbed();
   embedBuilder.title = "Help! I'm confused!";
   embedBuilder.description =
-      "As Pyrite is currently in it's very alpha stages, there are no help documents, or a support server quite yet! \n\n"
-      "Please stay tuned as development advances, a support server will be made and linked here soon!";
+      "Need help with Pyrite? Join the HQ server here to get some help: https://discord.gg/xzeWEDu4mj";
   embedBuilder.footer = EmbedFooterBuilder(text: "Guild ID: ${interaction.guild_id.toString()}");
 
   InteractionResponse response = InteractionResponse(InteractionResponseType.message_response, {
