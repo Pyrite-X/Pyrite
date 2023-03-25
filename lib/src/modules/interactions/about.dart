1d7ef0aca5b2e8d154bf077c456b24c69ad26e5d
Nub <38899321+One-Nub@users.noreply.github.com>
2023-03-24 23:24:54 -0500

    Link to repository in about.dart

--- lib/src/modules/interactions/about.dart
+++ lib/src/modules/interactions/about.dart
@@ -15,16 +15,24 @@ void aboutCmd(Interaction interaction) async {
       "https://cdn.discordapp.com/avatars/1022370218489692222/5966690e72baa4b1ceca1b76c49ef0ed.webp?size=2048";
   embedBuilder.addField(
       name: "What is Pyrite?", content: "Pyrite is a bot focused on removing phishing bots!", inline: false);
+
   embedBuilder.addField(
       name: "Why make Pyrite?",
       content:
           "While doing my stuff for [Bloxlink](https://blox.link), I noticed that a lot of large servers typically have many fake user accounts appearing like popular verification bots attempting to phish users (aka steal their account).\nSo just while thinking about why this is, I thought, why not make a bot that removes these accounts! And so, Pyrite came to be.",
       inline: false);
+
   embedBuilder.addField(
       name: "What is Pyrite coded in?",
       content:
           "[Dart!](https://dart.dev) With some self-made frameworks of my own, specifically [Lirx](https://github.com/One-Nub/Lirx) and [Onyx](https://github.com/One-Nub/Onyx). I also use [Nyxx](https://github.com/nyxx-discord/nyxx) for some gateway functionality, as well as for object representations.",
       inline: false);
+
+  embedBuilder.addField(
+      name: "Is Pyrite open source?",
+      content: "It is! Check it out here: https://github.com/Pyrite-X/Pyrite",
+      inline: false);
+
   embedBuilder.addFooter((footer) {
     footer.text = "Created by: Nub#8399 | Guild ID: ${interaction.guild_id}";
     footer.iconUrl =
