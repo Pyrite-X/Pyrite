50c5426e0afbe678c668986a7153ec80c0efd6b0
Nub <38899321+One-Nub@users.noreply.github.com>
2023-03-24 19:53:27 -0500

    Log guild joins with logging package

--- lib/src/modules/gateway/on_guild_create.dart
+++ lib/src/modules/gateway/on_guild_create.dart
@@ -1,8 +1,11 @@
+import 'package:logging/logging.dart';
 import 'package:nyxx/nyxx.dart';
 
 import '../../backend/database.dart' as db;
 
+Logger _logger = Logger("Guild Create");
 void on_guild_create(IGuildCreateEvent event) async {
-  print("Guild create event: ${event.guild.name} owned by ${event.guild.owner.id}");
+  _logger.info("Joined guild \"${event.guild.name}\" (${event.guild.id}), owner: ${event.guild.owner.id}");
+
   await db.insertNewGuild(serverID: BigInt.from(event.guild.id.id));
 }
