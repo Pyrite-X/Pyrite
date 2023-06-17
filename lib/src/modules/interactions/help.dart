ecfdcee8807882688642524401be8ad42d71ddef
Nub <38899321+One-Nub@users.noreply.github.com>
2023-06-17 16:45:40 -0500

    Add guide resources to the help command

--- lib/src/modules/interactions/help.dart
+++ lib/src/modules/interactions/help.dart
@@ -11,8 +11,11 @@ void helpCmd(Interaction interaction) async {
 
   EmbedBuilder embedBuilder = embeds.warningEmbed();
   embedBuilder.title = "Help! I'm confused!";
-  embedBuilder.description =
-      "Need help with Pyrite? Join the HQ server here to get some help: https://discord.gg/xzeWEDu4mj";
+  embedBuilder.description = "Need some help figuring out how to use Pyrite?\n\n"
+      "Try reading one of these guides:\n"
+      "1. [Setting up Pyrite](https://github.com/Pyrite-X/Pyrite/blob/main/guides/setting-up.md)\n"
+      "2. [Pyrite's main concepts](https://github.com/Pyrite-X/Pyrite/blob/main/guides/main-concepts.md)\n\n"
+      "Still confused? Join the [Support server](https://discord.gg/xzeWEDu4mj) for even more help!";
   embedBuilder.footer = EmbedFooterBuilder(text: "Guild ID: ${interaction.guild_id.toString()}");
 
   InteractionResponse response = InteractionResponse(InteractionResponseType.message_response, {
