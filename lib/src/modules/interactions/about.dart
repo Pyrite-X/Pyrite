453d11a4c66782d4ca366a84da576f0ed4f86bbf
Nub <38899321+One-Nub@users.noreply.github.com>
2023-06-17 16:59:14 -0500

    Update formatting for /about

--- lib/src/modules/interactions/about.dart
+++ lib/src/modules/interactions/about.dart
@@ -6,6 +6,8 @@ import 'package:onyx/onyx.dart';
 
 import '../../utilities/base_embeds.dart' as embeds;
 
+const String _unicodeBlank = "\u{2800}";
+
 void aboutCmd(Interaction interaction) async {
   HttpRequest request = interaction.metadata["request"];
 
@@ -14,18 +16,27 @@ void aboutCmd(Interaction interaction) async {
   embedBuilder.thumbnailUrl =
       "https://cdn.discordapp.com/avatars/1022370218489692222/5966690e72baa4b1ceca1b76c49ef0ed.webp?size=2048";
   embedBuilder.addField(
-      name: "What is Pyrite?", content: "Pyrite is a bot focused on removing phishing bots!", inline: false);
+      name: "What is Pyrite?",
+      content: "Pyrite is a bot focused on removing phishing bots!"
+          "\nIn simpler terms, Pyrite tries to remove user accounts who "
+          "try to mimic or look like large bots.\n${_unicodeBlank}",
+      inline: false);
 
   embedBuilder.addField(
       name: "Why make Pyrite?",
-      content:
-          "While doing my stuff for [Bloxlink](https://blox.link), I noticed that a lot of large servers typically have many fake user accounts appearing like popular verification bots attempting to phish users (aka steal their account).\nSo just while thinking about why this is, I thought, why not make a bot that removes these accounts! And so, Pyrite came to be.",
+      content: "While doing my stuff for [Bloxlink](https://blox.link), I noticed that a lot "
+          "of large servers typically have many fake user accounts appearing like "
+          "popular verification bots attempting to phish users (aka steal their account)."
+          "\nSo just while thinking about why this is, I thought, why not make a bot that removes these accounts! "
+          "And so, Pyrite came to be.\n${_unicodeBlank}",
       inline: false);
 
   embedBuilder.addField(
       name: "What is Pyrite coded in?",
-      content:
-          "[Dart!](https://dart.dev) With some self-made frameworks of my own, specifically [Lirx](https://github.com/One-Nub/Lirx) and [Onyx](https://github.com/One-Nub/Onyx). I also use [Nyxx](https://github.com/nyxx-discord/nyxx) for some gateway functionality, as well as for object representations.",
+      content: "[Dart!](https://dart.dev) With some self-made frameworks of my own, "
+          "specifically [Lirx](https://github.com/One-Nub/Lirx) and [Onyx](https://github.com/One-Nub/Onyx). "
+          "I also use [Nyxx](https://github.com/nyxx-discord/nyxx) for some gateway functionality, "
+          "as well as for object representations.\n${_unicodeBlank}",
       inline: false);
 
   embedBuilder.addField(
@@ -34,9 +45,7 @@ void aboutCmd(Interaction interaction) async {
       inline: false);
 
   embedBuilder.addFooter((footer) {
-    footer.text = "Created by: Nub#8399 | Guild ID: ${interaction.guild_id}";
-    footer.iconUrl =
-        "https://cdn.discordapp.com/avatars/156872400145874944/5c483c6328d43755bc4fa1f476d2e08e.webp";
+    footer.text = "Created by: Nub (@livesound) | Guild ID: ${interaction.guild_id}";
   });
 
   InteractionResponse response = InteractionResponse(InteractionResponseType.message_response, {
